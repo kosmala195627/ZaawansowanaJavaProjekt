795bf53e9967f72385af62ccbf32a6005df5c71d
bkosmala <b.kosmala@interia.pl>
2017-06-21 01:36:18 +0200

    Another unit test

--- Java2017/src/test/java/Database/Services/UsersServiceTest.java
+++ Java2017/src/test/java/Database/Services/UsersServiceTest.java
@@ -23,8 +23,17 @@ public class UsersServiceTest {
     } 
 	
 	@Test
-	public void testGetAllUsers(User[] tab) {
-		fail("Not yet implemented");
+	public void testGetAllUsers() {
+		User a1 = new User("Jan","Kowalski","jkowalski","$2a$10$6BhnrXJtEqAh13afhtWedeU4mxtWaxcyebQbbsnxJKxPPiWuV.IMW");
+		User a2 = new User("Karol","Bednarek","kbednarek","$2a$10$6BhnrXJtEqAh13afhtWedeU4mxtWaxcyebQbbsnxJKxPPiWuV.IMW");
+		User a3 = new User("Karol","Kłos","kk","$2a$10$6BhnrXJtEqAh13afhtWedeU4mxtWaxcyebQbbsnxJKxPPiWuV.IMW");
+		
+		User[] users = {a1,a2,a3};
+		String[] expected = {"Jan Kowalski","Karol Bednarek", "Karol Kłos"};
+		
+		String[] result = {};	
+		result = userService.getAllUsers(users);	
+		Assert.assertArrayEquals("Lista użytkowników niepoprawna", expected, result);
 	}
 
 }
